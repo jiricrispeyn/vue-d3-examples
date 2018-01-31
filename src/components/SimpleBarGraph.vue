2640f5f74079effce57fc850765110b8974f6dd9
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-01-31 20:34:03 +0100

    Setup dimension related properties

--- src/components/SimpleBarGraph.vue
+++ src/components/SimpleBarGraph.vue
@@ -1,10 +1,33 @@
 <template>
-  
+  <div class="simple-bar-graph">
+    <svg ref="svg">
+
+    </svg>
+  </div>
 </template>
 
 <script>
-export default {
+import { scaleBand, scaleLinear } from 'd3-scale';
+import { select } from 'd3-selection';
 
+export default {
+  name: 'simple-bar-graph',
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      margin: { top: 20, right: 20, bottom: 30, left: 40 },
+      x: scaleBand(),
+      y: scaleLinear()
+    }
+  },
+  mounted() {
+    this.width = this.$el.getBoundingClientRect().width - this.margin.left - this.margin.right;
+    this.height = this.$el.getBoundingClientRect().height - this.margin.top - this.margin.bottom;
+
+    this.x.range([0, this.width]);
+    this.y.range([this.height, 0]);
+  }
 }
 </script>
 
