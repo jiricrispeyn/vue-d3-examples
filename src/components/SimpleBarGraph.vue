4a88604cd183ac9a9a484f7332ec6e9ba59d295f
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-02-04 19:27:32 +0100

    Move x and y axis to correct position

--- src/components/SimpleBarGraph.vue
+++ src/components/SimpleBarGraph.vue
@@ -3,9 +3,9 @@
     <svg ref="svg" :width="width + margin.left + margin.right" :height="height + margin.top + margin.bottom">
       <g :style="{ 'transform': `translate(${margin.left}px, ${margin.top}px)` }">
         <rect class="bar" v-for="(item, index) in data" :key="index" :x="x(item.salesperson)" :width="x.bandwidth()" :y="y(item.sales)" :height="height - y(item.sales)" />
+        <g ref="xAxis" :style="{ 'transform': `translate(0, ${height}px)` }" />
+        <g ref="yAxis" />
       </g>
-      <g ref="xAxis" />
-      <g ref="yAxis" />
     </svg>
   </div>
 </template>
