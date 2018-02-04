e8fe0570977ee5e74153d54259fccc7942848bbf
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-02-04 18:33:17 +0100

    Add bar rects

--- src/components/SimpleBarGraph.vue
+++ src/components/SimpleBarGraph.vue
@@ -2,6 +2,7 @@
   <div class="simple-bar-graph">
     <svg ref="svg" :width="width + margin.left + margin.right" :height="height + margin.top + margin.bottom">
       <g :style="{ 'transform': `translate(${margin.left}px, ${margin.top}px)` }">
+        <rect class="bar" v-for="(item, index) in data" :key="index" :x="x(item.salesperson)" :width="x.bandwidth()" :y="y(item.sales)" :height="height - y(item.sales)" />
       </g>
     </svg>
   </div>
@@ -10,9 +11,16 @@
 <script>
 import { scaleBand, scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
+import { max } from 'd3-array';
 
 export default {
   name: 'simple-bar-graph',
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
       width: 0,
@@ -28,6 +36,20 @@ export default {
 
     this.x.range([0, this.width]);
     this.y.range([this.height, 0]);
+
+    this.update(this.data);
+  },
+  methods: {
+    update(data) {
+      this.x.domain(data.map(d => d.salesperson));
+      this.y.domain([0, max(data, d => d.sales)]);
+
+      this.xAxis.scale(this.x);
+      this.yAxis.scale(this.y);
+
+      select(this.$refs.xAxis).call(this.xAxis);
+      select(this.$refs.yAxis).call(this.yAxis);
+    }
   }
 }
 </script>
