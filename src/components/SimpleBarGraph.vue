8cee9d45ef7a302661c1d33357cc96b602dd7331
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-02-06 19:57:10 +0100

    Add getBarHeight function

--- src/components/SimpleBarGraph.vue
+++ src/components/SimpleBarGraph.vue
@@ -2,7 +2,7 @@
   <div class="simple-bar-graph">
     <svg ref="svg" :width="width + margin.left + margin.right" :height="height + margin.top + margin.bottom">
       <g :style="{ 'transform': `translate(${margin.left}px, ${margin.top}px)` }">
-        <rect class="bar" v-for="(item, index) in data" :key="index" :x="x(item.salesperson)" :width="x.bandwidth()" :y="y(item.sales)" :height="height - y(item.sales)" />
+        <rect class="bar" v-for="(item, index) in data" :key="index" :x="x(item.salesperson)" :width="x.bandwidth()" :y="y(item.sales)" :height="getBarHeight(item.sales)" />
         <g ref="xAxis" :style="{ 'transform': `translate(0, ${height}px)` }" />
         <g ref="yAxis" />
       </g>
@@ -55,6 +55,11 @@ export default {
 
       select(this.$refs.xAxis).call(this.xAxis);
       select(this.$refs.yAxis).call(this.yAxis);
+    },
+    getBarHeight(value) {
+      const height = this.height - this.y(value);
+
+      return height < 0 ? 0 : height;
     }
   }
 }
